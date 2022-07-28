7bb306f6d19e1faa4c478ec7eb635cb2be99ba36
pkrawat1 <pankajrawat19sept@gmail.com>
2022-07-28 17:04:24 +0530

    format code

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ fn main() {
 
     let mut blockchain = block::Blockchain::new();
     blockchain.genesis();
-    let lastest_block = blockchain.blocks.last().unwrap(); 
-    let new_block = block::Block::new(lastest_block.id + 1, "new".into(), lastest_block.hash.clone());
+    let lastest_block = blockchain.blocks.last().unwrap();
+    let new_block = block::Block::new(
+        lastest_block.id + 1,
+        "new".into(),
+        lastest_block.hash.clone(),
+    );
     blockchain.add_block(new_block);
 
     blockchain.print_blockchain();
