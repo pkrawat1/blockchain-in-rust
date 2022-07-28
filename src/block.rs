eb4380d25cc419645099066c25a387b1dc4e1e4b
pkrawat1 <pankajrawat19sept@gmail.com>
2022-07-28 17:03:14 +0530

    add block chain config

--- src/block.rs
+++ src/block.rs
@@ -1,31 +1,38 @@
 use chrono::UTC;
-use log::info;
+use log::{error, info, warn};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sha2::{Digest, Sha256};
 use std::fmt;
 
-#[derive(Debug, Clone)]
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
-    timestamp: i64,
-    data: String,
-    prev_block_hash: String,
+    pub id: u64,
+    pub timestamp: i64,
+    pub data: String,
+    pub prev_block_hash: String,
     pub hash: String,
 }
 
 impl Block {
-    pub fn new(data: String, prev_block_hash: String) -> Self {
+    pub fn new(id: u64, data: String, prev_block_hash: String) -> Self {
         let mut new_block = Self {
+            id,
             data,
             timestamp: UTC::now().timestamp(),
             prev_block_hash,
             hash: String::default(),
         };
-        new_block.hash = Block::mine_block(&new_block);
+        new_block.hash = new_block.block_hash();
         info!("mined! hash: {}", new_block.hash);
         new_block
     }
 
-    fn mine_block(&self) -> String {
+    fn block_hash(&self) -> String {
         let data = json!({
             "prev_block_hash": self.prev_block_hash,
             "data": self.data,
@@ -46,5 +53,50 @@ impl fmt::Display for Block {
             "({}, {}, {}, {})",
             self.timestamp, self.data, self.prev_block_hash, self.hash
         )
+    }
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+
+    pub fn genesis(&mut self) {
+        self.blocks
+            .push(Block::new(0, "genesis".into(), "genesis".into()));
+    }
+
+    pub fn add_block(&mut self, block: Block) {
+        let latest_block = self.blocks.last().expect("missing genesis block");
+        if self.block_valid(&block, &latest_block) {
+            self.blocks.push(block);
+        } else {
+            error!("Block[id:{}] is not a valid block", block.id);
+        }
+    }
+
+    pub fn print_blockchain(&self) {
+        info!("---Blockchain start---");
+        let pretty_json =
+            serde_json::to_string_pretty(&self.blocks).expect("failed to parse blocks");
+        info!("{}", pretty_json);
+        info!("---Blockchain end---");
+    }
+
+    fn block_valid(&self, block: &Block, prev_block: &Block) -> bool {
+        if block.prev_block_hash != prev_block.hash {
+            warn!("Block[id:{}] has invalid prev hash", block.id);
+            return false;
+        } else if block.id != prev_block.id + 1 {
+            warn!(
+                "Block[id:{}] does not come after Block[id:{}]",
+                block.id, prev_block.id
+            );
+            return false;
+        } else if block.hash != Block::block_hash(&block) {
+            warn!("Block[id:{}] has invalid hash", block.id);
+            return false;
+        }
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ mod block;
 
 fn main() {
     pretty_env_logger::init();
-    let genesis = block::Block::new("genesis".into(), "genesis".into());
-    let new_block = block::Block::new("Send 1 coin to User1".into(), genesis.hash.to_string());
-    println!("{}", genesis);
-    println!("{}", new_block);
+
+    let mut blockchain = block::Blockchain::new();
+    blockchain.genesis();
+    let lastest_block = blockchain.blocks.last().unwrap(); 
+    let new_block = block::Block::new(lastest_block.id + 1, "new".into(), lastest_block.hash.clone());
+    blockchain.add_block(new_block);
+
+    blockchain.print_blockchain();
 }
